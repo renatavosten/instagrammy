3c4cd7f09f66a3b3563295517839b802e24c1707
renatavosten <57946609+renatavosten@users.noreply.github.com>
2019-12-02 13:38:15 +0100

    Stuff added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,7 @@
 <template>
   <div class="row">
-    <div class="col-1"></div>
-    <div class="col-7">
-      <div class="home">
-        <InstagramCard/>
-        <InstagramCard/>
-      </div>
+    <div class="col-8">
+      <InstagramCard :key="card.id" :info="card" v-for="card in filteredCards" /> 
     </div>
     <div class="col-4">
       <InstagramStory/>
@@ -16,18 +12,34 @@
 
 <script>
 // @ is an alias to /src
-import InstagramCard from '@/components/InstagramCard.vue'
-import InstagramStory from '@/components/InstagramStory.vue'
-import InstagramSugg from '@/components/InstagramSugg.vue'
+import InstagramCard from '@/components/InstagramCard.vue';
+import InstagramStory from '@/components/InstagramStory.vue';
+import InstagramSugg from '@/components/InstagramSugg.vue';
+import store from "@/store.js";
 
 export default {
+  data () {
+    return store;  
+  },
+  computed: {
+    filteredCards() {
+      let filtered = [];
+      for (let card of this.cards) {
+        if (card.title.includes(this.searchTerm)) {
+          filtered.push(card);
+        }
+      }
+      return filtered;
+      //return this.cards.filter(card => card.title.includes(this.searchTerm)); malo drukčija sintaksa
+    }
+  },
   name: 'home',
   components: {
     InstagramCard,
     InstagramStory,
     InstagramSugg
   }
-}
+};
 </script>
 
 <style lang="scss">
