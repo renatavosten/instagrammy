dd1b9aa4dcd3b1818b347977c07dc1d906b8f9df
renatavosten <57946609+renatavosten@users.noreply.github.com>
2019-12-02 13:40:17 +0100

    Stuff added

---
+++ src/store.js
@@ -0,0 +1,11 @@
+export default {
+    authenticated: false,
+    userEmail: 'fake@email.com',
+    userName: 'Neki user',
+    cards: [
+        {id: 1, title: "jedan", time: "an hour ago", url: "photo-1574302833650-e91c6ec31969.jpg" },
+        {id: 2, title: "dva", time: "two hours ago", url: "photo-1574302833650-e91c6ec31969.jpg" },
+        {id: 3, title: "tri", time: "a day ago", url: "photo-1574302833650-e91c6ec31969.jpg" }
+    ],
+    searchTerm: ''
+}
