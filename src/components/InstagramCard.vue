e530a5bdb88a72f066b9f40bf712c0abc8014cc7
renatavosten <57946609+renatavosten@users.noreply.github.com>
2019-12-19 11:20:32 +0100

    Vue_05-include moment.js

--- src/components/InstagramCard.vue
+++ src/components/InstagramCard.vue
@@ -2,22 +2,27 @@
   <div class="card">
         <div class="card-header text-left">
           <img :src=info.avatar class="mr-3" alt ="..." />
-          <small><b>{{info.author }},</b> {{ info.title }}</small>
+          <small>
+            <b>{{ info.postedBy }}</b>
+          </small>
         </div>
       <div class="card-body p-0">
         <!-- p-0 je padding 0-->
-        <img @click="changeImage" class="card-img-top" :src="info.url" alt ="vrtuljak"/>
+        <img @click="changeImage" class="card-img-top" :src="info.url" alt ="..."/>
       </div>
-      <div class="card-footer text-muted text-left">
-        {{ info.time }}
-      </div>
+      <div class="card-footer text-muted text-left">{{ postedFromNow }}</div>
   </div>
-
 </template>
 
 <script>
 export default {
   props: ["info"],
+
+  computed: {
+    postedFromNow() {
+      return moment(this.info.time).fromNow();
+    }
+  },
   methods: {
     changeImage() {
       this.info.time = 'Fetching...'
