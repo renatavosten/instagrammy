08e6bf121b122b6158037553c39d0c3d6e3a65a6
renatavosten <57946609+renatavosten@users.noreply.github.com>
2019-12-19 11:20:57 +0100

    Week 5

--- src/store.js
+++ src/store.js
@@ -2,9 +2,10 @@ export default {
     authenticated: false,
     userEmail: 'fake@email.com',
     cards: [
-        {id: 1, avatar:"photo-1552168324-d612d77725e3.jpg", author: "photographymaster", title: "photo", time: "an hour ago", url: "slika8.jpg" },
-        {id: 2, avatar:"photo-1569077218751-3e3de7800c00.jpg", author:"foodblogger", title: "food", time: "two hours ago", url: "slika9.jpg" },
-        {id: 3, avatar:"photo-1574327694472-d172fb4d5865.jpg", author:"fashionmodels", title: "fashion", time: "a day ago", url: "slika10.jpg" }
+        {id: 1, avatar:"photo-1552168324-d612d77725e3.jpg", author: "photographymaster", postedBy: "rvosten@unipu.hr", time: "an hour ago", url: "slika8.jpg" },
+        {id: 2, avatar:"photo-1569077218751-3e3de7800c00.jpg", author:"foodblogger", postedBy: "rvosten@unipu.hr", time: "two hours ago", url: "slika9.jpg" },
+        {id: 3, avatar:"photo-1574327694472-d172fb4d5865.jpg", author:"fashionmodels", postedBy: "rvosten@unipu.hr", time: "a day ago", url: "slika10.jpg" }
     ],
-    searchTerm: ''
+    searchTerm: '',
+    postImageUrl: ''
 }
