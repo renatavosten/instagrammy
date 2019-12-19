b19087b96fcb7a50d3005b3d9b24aac3b8c44239
renatavosten <57946609+renatavosten@users.noreply.github.com>
2019-12-19 11:15:14 +0100

    Revert "Vue-05-moment.js"
    This reverts commit a24a8238a129b30324f919b36f65bb97f2ffc327.

--- src/components/InstagramCard.vue
+++ src/components/InstagramCard.vue
@@ -2,27 +2,22 @@
   <div class="card">
         <div class="card-header text-left">
           <img :src=info.avatar class="mr-3" alt ="..." />
-          <small>
-            <b>{{ info.postedBy }}</b>
-          </small>
+          <small><b>{{info.author }},</b> {{ info.title }}</small>
         </div>
       <div class="card-body p-0">
         <!-- p-0 je padding 0-->
-        <img @click="changeImage" class="card-img-top" :src="info.url" alt ="..."/>
+        <img @click="changeImage" class="card-img-top" :src="info.url" alt ="vrtuljak"/>
       </div>
-      <div class="card-footer text-muted text-left">{{ postedFromNow }}</div>
+      <div class="card-footer text-muted text-left">
+        {{ info.time }}
+      </div>
   </div>
+
 </template>
 
 <script>
 export default {
   props: ["info"],
-
-  computed: {
-    postedFromNow() {
-      return moment(this.info.time).fromNow();
-    }
-  },
   methods: {
     changeImage() {
       this.info.time = 'Fetching...'
